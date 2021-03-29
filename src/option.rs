8f684986d98705d3a92448bf548ebb87d0ef9434
Heinz N. Gies <heinz@licenser.net>
2021-03-29 12:08:52 +0200

    Implement ValueAccess for &V
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/option.rs
+++ src/option.rs
@@ -76,3 +76,44 @@ where
         self.as_ref().ok().and_then(|v| v.as_object())
     }
 }
+
+impl<V> ValueAccess for &V
+where
+    V: ValueAccess,
+{
+    type Target = V::Target;
+
+    type Key = V::Key;
+
+    type Array = V::Array;
+
+    type Object = V::Object;
+
+    fn as_bool(&self) -> Option<bool> {
+        (*self).as_bool()
+    }
+
+    fn as_i64(&self) -> Option<i64> {
+        (*self).as_i64()
+    }
+
+    fn as_u64(&self) -> Option<u64> {
+        (*self).as_u64()
+    }
+
+    fn as_f64(&self) -> Option<f64> {
+        (*self).as_f64()
+    }
+
+    fn as_str(&self) -> Option<&str> {
+        (*self).as_str()
+    }
+
+    fn as_array(&self) -> Option<&Self::Array> {
+        (*self).as_array()
+    }
+
+    fn as_object(&self) -> Option<&Self::Object> {
+        (*self).as_object()
+    }
+}
