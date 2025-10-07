2be6b7943affa588b77557e298c7425b29e4b644
Heinz N. Gies <heinz@licenser.net>
2025-10-07 10:39:20 +0200

    Obey clippy
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/node/cmp.rs
+++ src/node/cmp.rs
@@ -4,7 +4,6 @@ use super::StaticNode;
 
 impl PartialEq<()> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, _other: &()) -> bool {
         self.is_null()
     }
@@ -12,7 +11,6 @@ impl PartialEq<()> for StaticNode {
 
 impl PartialEq<bool> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &bool) -> bool {
         self.as_bool().is_some_and(|t| t.eq(other))
     }
@@ -20,7 +18,6 @@ impl PartialEq<bool> for StaticNode {
 
 impl PartialEq<str> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &str) -> bool {
         self.as_str().is_some_and(|t| t.eq(other))
     }
@@ -28,7 +25,6 @@ impl PartialEq<str> for StaticNode {
 
 impl PartialEq<&str> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &&str) -> bool {
         self == *other
     }
@@ -36,7 +32,6 @@ impl PartialEq<&str> for StaticNode {
 
 impl PartialEq<String> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &String) -> bool {
         self.as_str().is_some_and(|t| t.eq(other))
     }
@@ -44,7 +39,6 @@ impl PartialEq<String> for StaticNode {
 
 impl PartialEq<i8> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &i8) -> bool {
         self.as_i8().is_some_and(|t| t.eq(other))
     }
@@ -52,7 +46,6 @@ impl PartialEq<i8> for StaticNode {
 
 impl PartialEq<i16> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &i16) -> bool {
         self.as_i16().is_some_and(|t| t.eq(other))
     }
@@ -60,7 +53,6 @@ impl PartialEq<i16> for StaticNode {
 
 impl PartialEq<i32> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &i32) -> bool {
         self.as_i32().is_some_and(|t| t.eq(other))
     }
@@ -68,7 +60,6 @@ impl PartialEq<i32> for StaticNode {
 
 impl PartialEq<i64> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &i64) -> bool {
         self.as_i64().is_some_and(|t| t.eq(other))
     }
@@ -76,7 +67,6 @@ impl PartialEq<i64> for StaticNode {
 
 impl PartialEq<i128> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &i128) -> bool {
         self.as_i128().is_some_and(|t| t.eq(other))
     }
@@ -84,7 +74,6 @@ impl PartialEq<i128> for StaticNode {
 
 impl PartialEq<u8> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &u8) -> bool {
         self.as_u8().is_some_and(|t| t.eq(other))
     }
@@ -92,7 +81,6 @@ impl PartialEq<u8> for StaticNode {
 
 impl PartialEq<u16> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &u16) -> bool {
         self.as_u16().is_some_and(|t| t.eq(other))
     }
@@ -100,7 +88,6 @@ impl PartialEq<u16> for StaticNode {
 
 impl PartialEq<u32> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &u32) -> bool {
         self.as_u32().is_some_and(|t| t.eq(other))
     }
@@ -108,7 +95,6 @@ impl PartialEq<u32> for StaticNode {
 
 impl PartialEq<u64> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &u64) -> bool {
         self.as_u64().is_some_and(|t| t.eq(other))
     }
@@ -116,7 +102,6 @@ impl PartialEq<u64> for StaticNode {
 
 impl PartialEq<usize> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &usize) -> bool {
         self.as_usize().is_some_and(|t| t.eq(other))
     }
@@ -124,7 +109,6 @@ impl PartialEq<usize> for StaticNode {
 
 impl PartialEq<u128> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &u128) -> bool {
         self.as_u128().is_some_and(|t| t.eq(other))
     }
@@ -132,14 +116,12 @@ impl PartialEq<u128> for StaticNode {
 
 impl PartialEq<f32> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &f32) -> bool {
         self.as_f32().is_some_and(|t| t.eq(other))
     }
 }
 impl PartialEq<f64> for StaticNode {
     #[inline]
-    #[must_use]
     fn eq(&self, other: &f64) -> bool {
         self.as_f64().is_some_and(|t| t.eq(other))
     }
